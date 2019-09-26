d2a2b50f1d98ea0b44e71085b52f444077190d64
mjarraya <mjarraya@student.42.fr>
2019-09-26 11:28:20 +0200

    projects

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import Home from "./components/Home";
 import About from "./components/About";
-
+import Navbar from "./components/Navbar";
+import { Route, Switch } from "react-router-dom";
+import { Projects } from "./components/Projects";
 import "./App.css";
+
+const NotFound = () => {
+  return <div>Page not found</div>;
+};
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <About />
+      <Navbar />
+      <Switch>
+        {/* Switch renders the first child that matches the path */}
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   );
 }
